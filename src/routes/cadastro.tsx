4bf4bb04db5f2695b28b0055b2f3fd61f386c014
unknown <bandeira.freitas2013@gmail.com>
2023-11-09 23:37:09 -0300

    Validação disabled

--- src/routes/cadastro.tsx
+++ src/routes/cadastro.tsx
@@ -11,7 +11,9 @@ function Cadastro() {
     const [localidade, setLocalidade] = useState('');
     const [logradouro, setLogradouro] = useState('');
     const [bairro, setBairro] = useState('');
-
+    console.log();
+    
+    const [isFormDisabled, setIsFormDisabled] = useState(true);
   
     const checarCep = (e) =>{
         const cep = e.target.value.replace(/\D/g,'');
@@ -21,6 +23,11 @@ function Cadastro() {
             setLocalidade(data.localidade);
             setLogradouro(data.logradouro);
             setBairro(data.bairro);
+            setIsFormDisabled(true);
+
+            if(data.erro){
+                setIsFormDisabled(false);                     
+            }
         });
     }
   
@@ -30,13 +37,13 @@ function Cadastro() {
         <Form>
             <Row className="mb-3">
                 <Form.Group as={Col} controlId="formGridnome">
-                <Form.Label >Nome completo</Form.Label>
-                <Form.Control required type="text" placeholder="Digite seu nome completo" />
+                    <Form.Label >Nome completo</Form.Label>
+                    <Form.Control required type="text" placeholder="Digite seu nome completo" />
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridApelido">
-                <Form.Label>Apelido</Form.Label>
-                <Form.Control type="text" placeholder="Digite seu apelido" />
+                    <Form.Label>Apelido</Form.Label>
+                    <Form.Control type="text" placeholder="Digite seu apelido" />
                 </Form.Group>
             </Row>
 
@@ -49,30 +56,58 @@ function Cadastro() {
                         aria-label="Default"
                         aria-describedby="inputCep"
                     />
-                </InputGroup>
+            </InputGroup>
 
             <Row className="mb-3">
-            
-                <Form.Group as={Col} controlId="formGridState" >
-                <Form.Label>Estado</Form.Label>
-                <Form.Control required disabled value={uf}/>
+                <Form.Group as={Col} controlId="formBasicSelect" id='formUf'>
+                    <Form.Label>Estado</Form.Label>
+                    <Form.Control required disabled={isFormDisabled} value={uf} type = "text" as="select" >
+                        <option value="" selected>Selecione um estado</option>
+                        <option value="AC">AC</option>
+                        <option value="AL">AL</option>
+                        <option value="AP">AP</option>
+                        <option value="AM">AM</option>
+                        <option value="BA">BA</option>
+                        <option value="CE">CE</option>
+                        <option value="DF">DF</option>
+                        <option value="ES">ES</option>
+                        <option value="GO">GO</option>
+                        <option value="MA">MA</option>
+                        <option value="MT">MT</option>
+                        <option value="MS">MS</option>
+                        <option value="MG">MG</option>
+                        <option value="PA">PA</option>
+                        <option value="PB">PB</option>
+                        <option value="PR">PR</option>
+                        <option value="PE">PE</option>
+                        <option value="PI">PI</option>
+                        <option value="RJ">RJ</option>
+                        <option value="RN">RN</option>
+                        <option value="RS">RS</option>
+                        <option value="RO">RO</option>
+                        <option value="RR">RR</option>
+                        <option value="SC">SC</option>
+                        <option value="SP">SP</option>
+                        <option value="SE">SE</option>
+                        <option value="TO">TO</option>
+                    </Form.Control >
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridCity">
-                <Form.Label>Cidade</Form.Label>
-                <Form.Control required disabled value={localidade}/>
+                    <Form.Label>Cidade</Form.Label>
+                    <Form.Control required type="text" disabled={isFormDisabled} value={localidade}/>
                 </Form.Group>
             </Row>
 
             <Row className="mb-3">
                 <Form.Group as={Col}controlId="formGridLogradouro">
                     <Form.Label>Logradouro</Form.Label>
-                    <Form.Control required disabled value={logradouro}/>
+                    <Form.Control required type="text" disabled={isFormDisabled} value={logradouro}/>
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridBairro">
                     <Form.Label>Bairro</Form.Label>
-                    <Form.Control required disabled value={bairro}/>
+                    <Form.Control required type="text" disabled={isFormDisabled} value={bairro}/>
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="formGridNumero">
@@ -84,7 +119,7 @@ function Cadastro() {
 
             <Form.Group className="mb-3" controlId="formGridComplemento">
                 <Form.Label>Complemento</Form.Label>
-                <Form.Control required placeholder="Apartamento, Andar, etc" />
+                <Form.Control required type="text" placeholder="Apartamento, Andar, etc" />
             </Form.Group>
 
             <Button variant="primary" type="submit">
